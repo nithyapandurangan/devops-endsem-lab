6c7656f769ceccecaa2cce910dd0be08f2b1bde5
snucse <isha21110397@snuchennai.edu.in>
2024-11-08 11:06:06 +0530

    Update greeting message for testing 2

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -2,7 +2,7 @@ package com.example;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello, Jenkins and Docker and Git!");
+        System.out.println("Hello, Jenkins, Docker, Maven and Git!");
     }
 
     public int add(int a, int b) {
